a483b6972ccef1ed0a36c97bac4f11ed9fea70ee
Binkeurss <nvbinh271104@gmail.com>
2025-05-31 23:51:04 +0700

    commit logo coffee-shop-management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "../styles/globals.css";
 export const metadata: Metadata = {
   title: "BCoffee",
   description: "Coffee Shop Management",
+  icons: {
+    icon: '/images/logo_bcoffee.svg'
+  }
 };
 
 export default function RootLayout({
